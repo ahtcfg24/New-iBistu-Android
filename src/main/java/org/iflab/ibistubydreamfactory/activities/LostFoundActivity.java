2c3d25abb6ebf5e73420e17cdcb967d06c40ca38
DD-Windows <ahtcfg24@163.com>
2016-09-08 17:14:47 +0800

    统一ActionBar图标

--- src/main/java/org/iflab/ibistubydreamfactory/activities/LostFoundActivity.java
+++ src/main/java/org/iflab/ibistubydreamfactory/activities/LostFoundActivity.java
@@ -4,9 +4,11 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import org.iflab.ibistubydreamfactory.R;
@@ -30,15 +32,14 @@ public class LostFoundActivity extends AppCompatActivity {
      */
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("失物招领");
-        toolbar.setLogo(R.drawable.ic_action_logo_bistu);//设置静态logo
         setSupportActionBar(toolbar);//把ToolBar设置为ActionBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_navigation_arrow_back);
     }
 
     private void initView() {
         fragmentManager = getFragmentManager();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingButton);
-        fab.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.floatingButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 switchToPost();
@@ -52,11 +53,27 @@ public class LostFoundActivity extends AppCompatActivity {
     private void switchToPost() {
         // 开启Fragment管理事务
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        Fragment postFragment = new PostLostFoundFragment();
+        Fragment postFragment = PostLostFoundFragment.newInstance();
         transaction.add(R.id.fragment_container, postFragment);
         transaction.addToBackStack(null);//把fragment添加到回退栈中
-        // 提交事务
         transaction.commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_lost_found, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
--- src/main/java/org/iflab/ibistubydreamfactory/fragment/PostLostFoundFragment.java
+++ src/main/java/org/iflab/ibistubydreamfactory/fragment/PostLostFoundFragment.java
@@ -1,106 +1,44 @@
 package org.iflab.ibistubydreamfactory.fragment;
 
 import android.app.Fragment;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import org.iflab.ibistubydreamfactory.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PostLostFoundFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PostLostFoundFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PostLostFoundFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Button postButton;
+    private View rootView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public PostLostFoundFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PostLostFoundFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PostLostFoundFragment newInstance(String param1, String param2) {
-        PostLostFoundFragment fragment = new PostLostFoundFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static PostLostFoundFragment newInstance() {
+        return new PostLostFoundFragment();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_lost_found, container, false);
+        rootView = inflater.inflate(R.layout.fragment_post_lost_found, container, false);
+        initView();
+
+        return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    private void initView() {
+        postButton = (Button) rootView.findViewById(R.id.button_post);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
-        }
-    }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
